839ff10dede1f0089650560c9d2c4e878685c4b0
RMSNET <subscribe@ramsescaldas.com>
2022-01-20 14:29:17 -0300

    Update PQuery01.js

--- PQuery01.js
+++ PQuery01.js
@@ -1,6 +1,7 @@
 var http = require('http');
 var url = require('url');
 
+
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/html'});
   var q = url.parse(req.url, true).query;
